2dda6e57c70ccd51123e103340266701adc3922c
ankrish004 <44492616+ankrish004@users.noreply.github.com>
2025-10-04 20:30:11 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region =  "us-east-1"
+}
+
+locals {
+  env = terraform.workspace
+}
+
+resource "aws_s3_bucket" "ank5352" {
+    bucket =  "ank5352-${local.env}"
+    acl = "private"
+  
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.ank5352.bucket
+}
